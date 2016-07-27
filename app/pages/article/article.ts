04aad0eb9409c74c97573d8b4a4628bd0d5d4f79
latest <charles.jacquin@autistici.org>
2016-07-27 02:29:03 +0200

    add inAppBrowser support

--- app/pages/article/article.ts
+++ app/pages/article/article.ts
@@ -1,27 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ElementRef } from '@angular/core';
 import { NavParams } from 'ionic-angular';
+import { InAppBrowser } from 'ionic-native';
 import { Article, CommentStore, Comment } from '../../providers/store'
 import { ArticleWidgetFooterOptions } from '../../providers/directives/article-widget-footer';
 
 @Component({
   templateUrl: 'build/pages/article/article.html',
 })
-export class ArticlePageComponent implements OnInit {
+export class ArticlePageComponent implements OnInit, OnDestroy {
   article: Article;
+  links: NodeList;
   footerOptions: ArticleWidgetFooterOptions;
 
   constructor(
     private commentStore: CommentStore,
+    private ref: ElementRef,
     navParams: NavParams
   ) {
-    this.article = navParams.get('article');
-  }
 
-  ngOnInit() {
+    this.article = navParams.get('article');
+
     this.footerOptions = {
       social: true,
       comments: true,
       article: this.article
+    }
+  }
+
+  private clickLinkHandler(e): void {
+    e.preventDefault();
+    console.log(e.target);
+    InAppBrowser.open(e.target.getAttribute('href'));
+  }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.links = this.ref.nativeElement.querySelectorAll('a');
+      for (let i = 0; i < this.links.length; i++) {
+        this.links.item(i).addEventListener('click', this.clickLinkHandler)
+      }
+    })
+  }
+
+  ngOnDestroy(): void {
+    for (let i = 0; i < this.links.length; i++) {
+      this.links.item(i).removeEventListener('click', this.clickLinkHandler)
     }
   }
 }
