92884691cbdb3441fdcf2e3c6cbbbf2bf6455b6b
latest <charles.jacquin@autistici.org>
2016-07-31 13:48:46 +0200

    fix html-escape

--- app/utils/html-escape/index.ts
+++ app/utils/html-escape/index.ts
@@ -1,18 +1,19 @@
 export class HtmlEscape {
   static escape(source: string): string {
     const entityMap = {
-      "&": "&amp;",
-      "<": "&lt;",
-      ">": "&gt;",
+      '&': '&amp;',
+      '<': '&lt;',
+      '>': '&gt;',
       '"': '&quot;',
-      "'": '&#39;',
-      "/": '&#x2F;'
+      '\'': '&#39;',
+      '/': '&#x2F;'
     };
-    return String(source).replace(/[&<>"'\/]/g, s => entityMap[s]);
+    return String(source).replace(/[&<>''\/]/g, s => entityMap[s]);
   }
   static unescape(source: string): string {
     return new DOMParser()
-    .parseFromString(source, "text/html")
-    .documentElement.textContent;
+      .parseFromString(source, 'text/html')
+      .documentElement.textContent;
   }
 }
+''''''''''''''''''
